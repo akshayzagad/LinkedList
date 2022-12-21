67e17628458d5499f9875753b31add177285bc2a
akshayzagad <akshayzagade62@gmail.com.com>
2022-12-21 18:58:13 +0530

    Added:-Creat simple Linked List And Added Link

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -2,6 +2,7 @@ package com.bridgelabz;
 
 public class LinkedListMain {
     public static void main(String[] args) {
+        System.out.println("Welcome To My LinkedList");
         LinkedList<Integer> linkList1 = new LinkedList<>();
         linkList1.push(70);
         linkList1.push(30);
